c816f0613787209ed45eda8ca75f2310ba5738af
Zhilei Zhao <kimizhao9167@gmail.com>
2015-04-12 14:19:47 +0800

    programming_python_part1

---
+++ basics/chinese.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#put this line to enable use chinese
+print "你好！"
+print "Hello World!"
+print "Hello again!"
+print "error test."
+print "how do python deal with 'single quote' "
+print "quote in quote is not supported"
